bdc188b89e18c2f55b16384d1415e839fc551938
Pablo Pelardas <pablo.pelardas@gmail.com>
2025-05-09 17:09:37 -0300

    version inicial

---
+++ src/main.py
@@ -0,0 +1,43 @@
+
+filePath = './data/preguntas.txt'
+
+def obtener_preguntas():
+    preguntas = []
+    with open(filePath, 'r', encoding='utf-8') as archivo:
+        for linea in archivo:
+            linea = linea.strip()
+            if '|' in linea:
+                pregunta, respuesta = linea.split('|', 1)  # divide solo en el primer '|'
+                preguntas.append({
+                    'pregunta': pregunta.strip(),
+                    'respuesta': respuesta.strip()
+                })
+    return preguntas
+
+def escribir_pregunta(pregunta):
+    with open(filePath, 'a', encoding='utf-8') as archivo:
+        archivo.write(f'{pregunta} | Sin respuesta\n')
+        
+def buscar_respuesta(preguntas,pregunta):
+    for p in preguntas:
+        if p['pregunta'] == pregunta:
+            return p['respuesta']
+    escribir_pregunta(pregunta)
+    preguntas.append({
+        'pregunta': pregunta,
+        'respuesta': 'Sin respuesta'
+    })
+    return 'Sin respuesta'
+
+def main():
+    preguntas = obtener_preguntas()
+    while True:
+        pregunta = input('¿Cuál es tu pregunta? (Escribe "salir" para terminar): ')
+        if pregunta.lower() == 'salir':
+            break
+        respuesta = buscar_respuesta(preguntas,pregunta)
+        print(f'Respuesta: {respuesta}')
+
+
+if __name__ == '__main__':
+    main() 
